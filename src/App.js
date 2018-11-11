98d09594fffb38fe84799ab1148aa6e2be0563ee
DirectX <DirectX@users.noreply.github.com>
2018-11-11 19:00:35 +0300

    UI update, block load error handling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import { Container, Nav, Table, Row, Col } from 'reactstrap';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight, faSitemap } from '@fortawesome/free-solid-svg-icons'
+import { Container, Nav, Table, Row, Col,Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 import { TxTypeSplit, TxTypeMerge, TxTypeFund, Block } from '@thematter_io/plasma.js';
 import Web3 from 'web3';
 import ethUtil from 'ethereumjs-util';
 import logo from './logo.svg';
 import './App.css';
+
+library.add(faArrowRight, faSitemap);
 
 class App extends Component {
   constructor(props) {
@@ -52,10 +57,10 @@ class App extends Component {
 
   formatPrice(weiPriceString) {
     let price = this.state.web3.utils.fromWei(weiPriceString);
-    if (price >= '0.0001') {
+    if (price >= '0.00001') {
       return `${price} ETH`;
     } else {
-      return `${weiPriceString} Wei`
+      return `${weiPriceString} Wei`;
     }
   }
 
@@ -80,21 +85,22 @@ class App extends Component {
 
       const reader = new FileReader();
       reader.addEventListener("loadend", async function () {
-        const blockArrayBuffer = this.result;
-        const blockBuffer = new Buffer(blockArrayBuffer);
-        const block = new Block(blockBuffer);
-        const blockObject = block.toFullJSON(true);
-        console.log(blockObject);
-
-        const blockInfo = {
-          parentHash: ethUtil.bufferToHex(blockObject.header.parentHash),
-          merkleRootHash: ethUtil.bufferToHex(blockObject.header.merkleRootHash),
-          hash: ethUtil.bufferToHex(block.header.hash()),
-          from: self.state.web3.utils.toChecksumAddress(ethUtil.bufferToHex(block.header.getSenderAddress())),
-          txCount: block.transactions.length
-        };
-
-        self.setState({ blockNumber: blockNumber, blockInfo: blockInfo, block: blockObject });
+        try {
+          const blockArrayBuffer = this.result;
+          const blockBuffer = new Buffer(blockArrayBuffer);
+          const block = new Block(blockBuffer);
+          const blockObject = block.toFullJSON(true);
+          const blockInfo = {
+            parentHash: ethUtil.bufferToHex(blockObject.header.parentHash),
+            merkleRootHash: ethUtil.bufferToHex(blockObject.header.merkleRootHash),
+            hash: ethUtil.bufferToHex(block.header.hash()),
+            from: self.state.web3.utils.toChecksumAddress(ethUtil.bufferToHex(block.header.getSenderAddress())),
+            txCount: block.transactions.length
+          };
+          self.setState({ blockNumber: blockNumber, blockInfo: blockInfo, block: blockObject });
+        } catch (e) {
+          console.log(e);
+        }
       });
       reader.readAsArrayBuffer(blockBlob);
     } 
@@ -114,92 +120,108 @@ class App extends Component {
         </div>
         <Container>
           <h2 className="display-4">Blocks</h2>
-          <Row>
-            <Col sm={2}>
-              {this.state.blockNumbers.map(function (blockNumber) {
-                return <Container className={"block-number p-2 shadow-sm" + (blockNumber === this.state.blockNumber ? " active" : "")} onClick={() => this.setBlockNumber(blockNumber)}>
-                  Block {blockNumber}
-                </Container>
-              }, this)}
-            </Col>
-            <Col hidden={this.state.blockNumber === 0}>
-              <h3>Block {this.state.blockNumber}</h3>
-              <h4>Information</h4>
-              <Table striped className="info">
-                <thead>
-                  <tr>
-                    <th>Property</th>
-                    <th>Value</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">Merkle Root Hash</th>
-                    <td>{this.state.blockInfo.merkleRootHash}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Parent Hash</th>
-                    <td onClick={() => this.setBlockNumber(this.state.blockNumber - 1)} className="cursor-pointer">{this.state.blockInfo.parentHash}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Hash</th>
-                    <td>{this.state.blockInfo.hash}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Creator</th>
-                    <td>{this.state.blockInfo.from}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Transaction Count</th>
-                    <td>{this.state.blockInfo.txCount}</td>
-                  </tr>
-                </tbody>
-              </Table>
-              <h4>Transactions</h4>
-              <Table striped className="transactions">
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Type</th>
-                    <th>Inputs</th>
-                    <th>Outputs</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.state.block.transactions.map(function (transaction, transactionIndex) {
-                    return <tr>
-                      <th scope="row">{transactionIndex}</th>
-                      <td>{this.formatTransactionType(transaction.transaction.transactionType)}</td>
-                      <td>
-                        {transaction.transaction.inputs.map(function (input, inputIndex) {
-                          return <Row>
-                            <Col className="tx-input px-3 py-2 mx-3 mb-1">
+          <Pagination aria-label="Page navigation example" className="mb-5">
+            <PaginationItem disabled>
+              <PaginationLink previous href="#" />
+            </PaginationItem>
+            {this.state.blockNumbers.map(function (blockNumber) {
+              return <PaginationItem active={blockNumber === this.state.blockNumber}>
+                <PaginationLink onClick={() => this.setBlockNumber(blockNumber)}>
+                  {blockNumber}
+                </PaginationLink>
+              </PaginationItem>
+            }, this)}
+            <PaginationItem disabled>
+              <PaginationLink next href="#" />
+            </PaginationItem>
+          </Pagination>
+          <div hidden={this.state.blockNumber === 0}>
+            <h3>Block {this.state.blockNumber} Information</h3>
+            <Table striped className="info">
+              <thead>
+                <tr>
+                  <th>Property</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Merkle Root Hash</th>
+                  <td>{this.state.blockInfo.merkleRootHash}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Parent Hash</th>
+                  <td onClick={() => this.setBlockNumber(this.state.blockNumber - 1)} className="cursor-pointer">{this.state.blockInfo.parentHash}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Hash</th>
+                  <td>{this.state.blockInfo.hash}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Creator</th>
+                  <td>{this.state.blockInfo.from}</td>
+                </tr>
+                <tr>
+                  <th scope="row">Transaction Count</th>
+                  <td>{this.state.blockInfo.txCount}</td>
+                </tr>
+              </tbody>
+            </Table>
+            <h4>Transactions</h4>
+            <Table striped className="transactions">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Type</th>
+                  <th>Inputs</th>
+                  <th></th>
+                  <th>Outputs</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.block.transactions.map(function (transaction, transactionIndex) {
+                  return <tr>
+                    <th scope="row">{transactionIndex}</th>
+                    <td>{this.formatTransactionType(transaction.transaction.transactionType)}</td>
+                    <td>
+                      {transaction.transaction.inputs.map(function (input, inputIndex) {
+                        return <Row>
+                          <Col className="tx-input shadow-sm px-3 py-2 mx-3 mb-1">
+                            <div hidden={transaction.transaction.transactionType === TxTypeFund}>
                               Input Number: <strong>{inputIndex}</strong><br />
                               Block Number: <strong onClick={() => this.setBlockNumber(input.blockNumber)} className="cursor-pointer">{input.blockNumber}</strong><br />
                               Output Number in Transaction: <strong>{input.outputNumberInTransaction}</strong><br />
                               Transaction Number in Block: <strong>{input.txNumberInBlock}</strong><br />
                               Value: <strong>{this.formatPrice(input.value)}</strong>
-                            </Col>
-                          </Row>
-                        }, this)}
-                      </td>
-                      <td>
-                        {transaction.transaction.outputs.map(function (output, outputIndex) {
-                          return <Row>
-                            <Col className="tx-input px-3 py-2 mx-3 mb-1">
-                              Output Number in Transaction: <strong>{output.outputNumberInTransaction}</strong><br />
-                              To: <strong>{output.to}</strong><br />
-                              Value: <strong>{this.formatPrice(output.value)}</strong>
-                            </Col>
-                          </Row>
-                        }, this)}
-                      </td>
-                    </tr>
-                  }, this)}
-                </tbody>
-              </Table>
-            </Col>
-          </Row>
+                            </div>
+                            <div hidden={transaction.transaction.transactionType !== TxTypeFund}>
+                              Deposit Index: <strong>{input.value}</strong>
+                            </div>
+                          </Col>
+                        </Row>
+                      }, this)}
+                    </td>
+                    <td>
+                      <FontAwesomeIcon icon="sitemap" rotation={270} size="2x" hidden={transaction.transaction.transactionType !== TxTypeSplit}/>
+                      <FontAwesomeIcon icon="sitemap" rotation={90} size="2x" hidden={transaction.transaction.transactionType !== TxTypeMerge}/>
+                      <FontAwesomeIcon icon="arrow-right" size="2x" hidden={transaction.transaction.transactionType !== TxTypeFund}/>
+                    </td>
+                    <td>
+                      {transaction.transaction.outputs.map(function (output, outputIndex) {
+                        return <Row>
+                          <Col className="tx-output shadow-sm px-3 py-2 mx-3 mb-1">
+                            Output Number in Transaction: <strong>{output.outputNumberInTransaction}</strong><br />
+                            To: <strong>{output.to}</strong><br />
+                            Value: <strong>{this.formatPrice(output.value)}</strong>
+                          </Col>
+                        </Row>
+                      }, this)}
+                    </td>
+                  </tr>
+                }, this)}
+              </tbody>
+            </Table>
+          </div>
         </Container>
       </div>
     );
